b1637ef8cc8841e38062224385b3e4fabbd1faee
LeskovPV <LeskovPV@dv.rt.ru>
2019-06-05 17:25:31 +1200

    Ковыряние в Staffbot (по части копирования cfg)

--- src/main/java/ru/staffbot/Staffbot.java
+++ src/main/java/ru/staffbot/Staffbot.java
@@ -6,13 +6,14 @@ import ru.staffbot.database.journal.Journal;
 import ru.staffbot.utils.devices.Devices;
 import ru.staffbot.webserver.WebServer;
 import ru.staffbot.webserver.servlets.MainServlet;
+import ru.staffbot.webserver.servlets.ResourceServlet;
 import ru.staffbot.windows.MainWindow;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.OpenOption;
 import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardOpenOption;
 import java.util.Properties;
 
 
@@ -37,16 +38,44 @@ public abstract class Staffbot{
         } catch (IOException exception) {
             Journal.add("Ошибка чтения файла свойств проекта (properties):\n"+ exception.getMessage());
         }
+
         // Имя файла конфигурации
         String projectCfgFileName = "/" + projectName + ".cfg"; // внутри jar-пакета
         String solutionCfgFileName = "/" + solutionName + "-" + solutionVersion + ".cfg";
+        System.out.println(projectCfgFileName);
+        System.out.println(solutionCfgFileName);
         try {
             // Расположение файла конфигурации в той же директории, что и запускаемый jar-пакет
-            File cfgFile = new File((new File(Staffbot.class.getProtectionDomain().getCodeSource().getLocation().getPath())).getParent() + solutionCfgFileName);
+            String jarFileName = Staffbot.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+            System.out.println(jarFileName);
+            String jarDirName = (new File(jarFileName)).getParent();
+            System.out.println(jarDirName);
+            File cfgFile = new File((new File(jarFileName)).getParent() + solutionCfgFileName);
+            System.out.println(cfgFile.toPath());
             // Если файл конфигурации ещё не существует,
             if (!cfgFile.exists())
+            try {
                 // то копируем его из ресурсов, заархивированных внутри jar-файла
-                Files.copy(Staffbot.class.getResourceAsStream(projectCfgFileName), cfgFile.toPath(), StandardCopyOption.REPLACE_EXISTING);
+                System.out.println("Copy resource " + projectCfgFileName);
+                InputStream inputStream = Staffbot.class.getResourceAsStream(projectCfgFileName);
+                //InputStream inputStream = Staffbot.class.getResourceAsStream(resourceName);
+                System.out.println("to " + cfgFile.getPath());
+                FileOutputStream outputStream = new FileOutputStream(cfgFile.getPath());
+                System.out.println("before write");
+                outputStream.write(inputStream.readAllBytes());
+                System.out.println("OK - coped");
+
+                //File targetFile = new File(cfgFile.getPath());
+                //OpenOption[] options = new OpenOption[] { StandardOpenOption.WRITE, StandardOpenOption.CREATE };
+                //java.nio.file.Files.copy(inputStream,targetFile.toPath(),StandardCopyOption.REPLACE_EXISTING);
+                //FileUtils.copyInputStreamToFile(initialStream, targetFile);
+                } catch (Exception exception){
+
+                System.out.println("Error of coping: " + exception.getMessage());
+                System.out.println("StackTrace: " + exception.getStackTrace());
+
+            }
+
             // Читаем файл конфигурации
             FileInputStream inputStream = new FileInputStream(cfgFile);
             Properties property = new Properties();
